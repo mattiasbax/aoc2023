843d020e3f9871d24b7392959f8b08b61db17cbf
Mattias Bäckström <mattias.bax@live.se>
2023-12-04 16:45:42 +0100

    Day 4

---
+++ cpp/day4.hpp
@@ -0,0 +1,95 @@
+#pragma once
+
+#include "utility.hpp"
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+class Day4 {
+public:
+    Day4( ) = default;
+
+    std::pair<int, int> Solve( )
+    {
+        int sumPartOne = 0;
+        int sumPartTwo = 0;
+        int ticketNumber = 0;
+        std::unordered_map<int, int> copies;
+        processOnInput( date, [ & ]( auto s ) {
+            const auto st = convertString( s );
+            int numberOfWinningNumbers = numberOfwinningNumbers( st );
+
+            if ( numberOfWinningNumbers ) {
+                sumPartOne += static_cast<int>( std::pow( 2, numberOfWinningNumbers - 1 ) );
+            }
+
+            copies[ ticketNumber ]++;
+            sumPartTwo += copies[ ticketNumber ];
+            for ( int i = 0; i < numberOfWinningNumbers; ++i ) {
+                copies[ ticketNumber + 1 + i ] += copies[ ticketNumber ];
+            }
+            ++ticketNumber;
+        } );
+
+        return { sumPartOne, sumPartTwo };
+    }
+
+    struct ScratchTicket {
+        std::unordered_set<int> winningNumbers;
+        std::vector<int> numbers;
+    };
+
+    ScratchTicket convertString( const std::string& s )
+    {
+        ScratchTicket st;
+        auto begin = s.find( ':' );
+        auto end = s.find( '|', begin + 1 );
+        const std::string& winningNumbers = s.substr( begin + 2, end - begin - 3 );
+        const std::string& numbers = s.substr( end + 2, s.size( ) );
+
+        std::string temp;
+        std::istringstream winningNumberStream( winningNumbers );
+        while ( winningNumberStream >> temp ) {
+            st.winningNumbers.insert( std::stoi( temp ) );
+        }
+
+        std::istringstream numberStream( numbers );
+        while ( numberStream >> temp ) {
+            st.numbers.push_back( std::stoi( temp ) );
+        }
+
+        return st;
+    }
+
+    int numberOfwinningNumbers( const ScratchTicket& st )
+    {
+        int numberOfWinningNumbers = 0;
+        for ( int number : st.numbers ) {
+            if ( st.winningNumbers.contains( number ) ) {
+                ++numberOfWinningNumbers;
+            }
+        }
+        return numberOfWinningNumbers;
+    }
+
+private:
+    static constexpr int date = 4;
+};
+
+TEST( Day4, convertString )
+{
+    Day4 d4;
+    ASSERT_EQ( 4, d4.numberOfwinningNumbers( d4.convertString( "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53" ) ) );
+}
+
+TEST( Day4, Solve )
+{
+    Day4 d4;
+    const auto& [ partOne, partTwo ] = d4.Solve( );
+    ASSERT_EQ( partOne, 21105 );
+    ASSERT_EQ( partTwo, 5329815 );
+}
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,6 +1,7 @@
 #include "day1.hpp"
 #include "day2.hpp"
 #include "day3.hpp"
+#include "day4.hpp"
 
 #include <iostream>
 
