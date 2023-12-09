55c25f23a4ac2a347736b9fe29c03aac76bf30d5
Mattias Bäckström <mattias.bax@live.se>
2023-12-09 17:08:44 +0100

    Day 9 solution 2

--- cpp/day9.hpp
+++ cpp/day9.hpp
@@ -3,6 +3,7 @@
 #include "utility.hpp"
 
 #include <algorithm>
+#include <deque>
 #include <numeric>
 #include <ranges>
 #include <sstream>
@@ -27,37 +28,53 @@ public:
                     sequence.push_back( sequenceNumber );
 
                 sumPartOne += extraPolateForward( sequence );
-                sumPartTwo += extraPolateBackward( sequence );
+                sumPartTwo += extraPolateBackward( std::deque( sequence.begin( ), sequence.end( ) ) );
             },
             runOnTestInput
         );
-        return { sumPartOne, 2 };
+        return { sumPartOne, sumPartTwo };
     }
 
-    int extraPolateForward( std::vector<int> sequence )
+    static int extraPolateForward( std::vector<int> sequence )
     {
         std::vector<int> lastElements;
         while ( !std::ranges::all_of( sequence, []( auto& val ) { return val == 0; } ) ) {
             lastElements.push_back( sequence.back( ) );
-            for ( int i = 0; i < sequence.size( ) - 1; ++i ) {
-                sequence[ i ] = sequence[ i + 1 ] - sequence[ i ];
-            }
+            std::adjacent_difference( sequence.begin( ) + 1, sequence.end( ), sequence.begin( ) );
             sequence.pop_back( );
         }
         return std::accumulate( lastElements.begin( ), lastElements.end( ), 0 );
     }
 
-    int extraPolateBackward( std::vector<int> sequence ) { return 0; }
+    static int extraPolateBackward( std::deque<int> sequence )
+    {
+        std::deque<int> firstElements;
+        while ( !std::ranges::all_of( sequence, []( auto& val ) { return val == 0; } ) ) {
+            firstElements.push_front( sequence.front( ) );
+            std::adjacent_difference( sequence.begin( ), sequence.end( ), sequence.begin( ) );
+            sequence.pop_front( );
+        }
+        return std::accumulate( firstElements.begin( ), firstElements.end( ), 0, []( int lhs, int rhs ) {
+            return rhs - lhs;
+        } );
+    }
 
 private:
     static constexpr int date = 9;
 };
+
+TEST( Day9, extraPolateBackward )
+{
+    int result = Day9::extraPolateBackward( { 10, 13, 16, 21, 30, 45 } );
+    ASSERT_EQ( 5, result );
+}
 
 TEST( Day9, Test_1 )
 {
     Day9 d9;
     const auto [ partOne, partTwo ] = d9.Solve( 1 );
     ASSERT_EQ( partOne, 114 );
+    ASSERT_EQ( partOne, 2 );
 }
 
 TEST( Solve, day_9 )
@@ -65,5 +82,5 @@ TEST( Solve, day_9 )
     Day9 d9;
     const auto [ partOne, partTwo ] = d9.Solve( );
     ASSERT_EQ( partOne, 1898776583 );
-    ASSERT_EQ( partTwo, 2 );
+    ASSERT_EQ( partTwo, 1100 );
 }
